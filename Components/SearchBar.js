44ea203f5605841af892dda5d48ad36f337b6bf4
Jhonathan Rodrigues <jhonathan@iMac-de-Jhonathan.local>
2018-08-11 13:28:30 -0300

    Changes:
    Added a SearchBar / Created a SearchBar Component

---
+++ Components/SearchBar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            < View
+                style={{
+                    flexDirection: 'row',
+                    zIndex: 1,
+                    padding: 10,
+                    backgroundColor: 'white',
+                    marginHorizontal: 20,
+                    shadowOffset: { width: 0, height: 0 },
+                    shadowColor: 'black',
+                    shadowOpacity: 0.2,
+                    elevation: 1,
+                    marginTop: Platform.OS == 'android' ? 30 : null
+                }
+                }>
+
+                <Icon name="ios-search" size={20} style={{ top: 4 }} />
+                <TextInput
+                    underlineColorAndroid="transparent"
+                    placeholder="Try Simpsons"
+                    placeholderTextColor="grey"
+                    style={{
+                        flex: 1,
+                        fontWeight: '700',
+                        backgroundColor: 'white',
+                        marginLeft: 10
+                    }}
+                />
+            </View >
+        )
+    }
+}
+
+export default SearchBar;
--- Screens/Recipes.js
+++ Screens/Recipes.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Platform, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import SearchBar from '../Components/SearchBar';
 
-class Recipes extends Component{
-    render(){
-        return(
-            <View style ={styles.container}>
-                <Text>Recipes</Text>
+class Recipes extends Component {
+    render() {
+        return (
+            <View>
+                <SearchBar />
+
+                <View
+                    style={{
+                        paddingHorizontal: 20,
+                        marginTop: 20,
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                        backgroundColor: 'red'
+                    }}>
+
+                </View>
             </View>
         )
     }
